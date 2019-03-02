02c191660b08b09b4726954f3260bbe6c9090041
opp1opp1 <opp1opp1@yahoo.com.tw>
2019-03-02 20:16:39 +0800

    instantiate bullet&bullet self-destroyed
    新增角色發射子彈的功能
    還有子彈過一段時間會自毀

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+    public GameObject sphere;
+    public float Shoot_Pos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Instantiate(sphere, (this.gameObject.transform.position + new Vector3( Shoot_Pos,0f, 0f)), this.gameObject.transform.rotation);
+            sphere.gameObject.transform.rotation = this.gameObject.transform.rotation;
+        } 
+
+	}
+}
---
+++ Assets/bulletdestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletdestroy : MonoBehaviour {
+    private float lifeTime;
+    public float maxTime = 3.0f;
+    // Use this for initialization
+    void Start () {
+        lifeTime = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime += Time.deltaTime;
+        if (lifeTime > maxTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
