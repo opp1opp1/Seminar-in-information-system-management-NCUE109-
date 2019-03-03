078d3348838386674f1a22cafd89ac9e4fad386c
opp1opp1 <opp1opp1@yahoo.com.tw>
2019-03-03 10:24:04 +0800

    Limit Firerate and Limit Max Charge And Player Rotation
    新增射速限制
    新增最大充能數
    角色現在可以利用滑鼠旋轉

---
+++ Assets/PlayerRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+
+{
+    Vector3 targetPoint = Vector3.zero;     //鼠标点击的位置
+    
+
+    void Start()
+    {
+
+        targetPoint = transform.position;
+       
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                
+                //点击位置坐标   
+                targetPoint = hit.point;
+                //转向  
+                transform.LookAt(new Vector3(targetPoint.x, transform.position.y, targetPoint.z));
+            }
+        }
+
+        
+    }
+}
--- Assets/PlayerShoot.cs
+++ Assets/PlayerShoot.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour {
     public GameObject sphere;
     public float Shoot_Pos;
-	// Use this for initialization
-	void Start () {
-		
+    public float MaxCharge =3;
+    private float CheckFiretime = 0;
+    public float ShootRate =0.5f;
+    public float ChargeTime = 1f;
+    private float CheckChargetime = 0;
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyUp(KeyCode.Space))
+        CheckFiretime += Time.deltaTime;
+        if (Input.GetKeyUp(KeyCode.Space)&& CheckFiretime > ShootRate && MaxCharge>=1)
         {
             Instantiate(sphere, (this.gameObject.transform.position + new Vector3( Shoot_Pos,0f, 0f)), this.gameObject.transform.rotation);
             sphere.gameObject.transform.rotation = this.gameObject.transform.rotation;
-        } 
+            MaxCharge -= 1;
+            CheckFiretime = 0;
+        }
+        CheckChargetime += Time.deltaTime;
+        if (CheckChargetime >= ChargeTime)
+        {
+            MaxCharge += 1;
+            CheckChargetime = 0;
+        }
 
-	}
+    }
 }
