48c6b0d4ebd8739021be67e4b1ae99ee4b6f667c
opp1opp1 <opp1opp1@yahoo.com.tw>
2019-03-21 00:26:00 +0800

    瞄準功能
    應該吧

--- Assets/PlayerRotation.cs
+++ Assets/PlayerRotation.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class PlayerRotation : MonoBehaviour
 
 {
-    Vector3 targetPoint = Vector3.zero;     //鼠标点击的位置
-    
+     Vector3 targetPoint = Vector3.zero;     //鼠标点击的位置
+    public GameObject aiming_sprite;
 
     void Start()
     {
 
         targetPoint = transform.position;
-       
+
     }
 
     void Update()
@@ -27,15 +27,36 @@ public class PlayerRotation : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
 
-                
+
                 //点击位置坐标   
                 targetPoint = hit.point;
                 //转向  
                 transform.LookAt(new Vector3(targetPoint.x, transform.position.y, targetPoint.z));
-                
+                aiming_sprite.transform.LookAt(new Vector3(targetPoint.x, transform.position.y, targetPoint.z));
             }
         }
+        if (Input.GetMouseButtonDown(0))
+        {
 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+
+                //点击位置坐标   
+                targetPoint = hit.point;
+                //转向  
+
+                Instantiate(aiming_sprite, (this.gameObject.transform.position + new Vector3(0f, 0.1f, 0f)), this.gameObject.transform.rotation);
+
+            }
+        }
         
     }
+    
+    
+  
+
 }
