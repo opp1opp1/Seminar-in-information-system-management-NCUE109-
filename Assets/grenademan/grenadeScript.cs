6566e5cfdf231141cf044c21a91bbe726a840170
Kevinkang0211 <45477381+Kevinkang0211@users.noreply.github.com>
2019-04-16 00:16:50 +0800

    新增一個丟手榴彈的人和功能

---
+++ Assets/grenademan/grenadeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenadeScript : MonoBehaviour {
+
+    public float timeDelay = 2f;
+    float startTimer;
+
+    public int damage = 10;
+    public float explosiveForce = 20f;
+    public float explosiveRadius = 15f;
+
+	// Update is called once per frame
+	void Update () {
+        startTimer += Time.deltaTime;
+            if(startTimer >= timeDelay){
+                    Explode();
+                }
+		
+	}
+
+    void Explode(){
+        Collider[] coll = Physics.OverlapSphere(transform.position, explosiveRadius);
+
+        for(int i =0; i < coll.Length; i++){
+            if (coll[i].gameObject.GetComponent<testDummy>())
+            {
+                coll[i].gameObject.GetComponent<testDummy>().TakeDamage(damage);
+                coll[i].gameObject.GetComponent<Rigidbody>().AddExplosionForce(explosiveForce, transform.position, explosiveRadius);
+            }
+        }
+        //Destroy(gameObject);
+    }
+   
+}
