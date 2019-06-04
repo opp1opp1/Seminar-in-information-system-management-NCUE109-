2e7bacd0a59dfcbd5bba821b78fe103aef894fee
opp1opp1 <opp1opp1@yahoo.com.tw>
2019-06-04 13:07:01 +0800

    導航Part1

---
+++ Assets/Enemy/EnemyBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour {
+    private NavMeshAgent agent;
+    public GameObject Player;
+    public float WakeUpDistance = 5.0f;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        if (distance < WakeUpDistance)
+        {
+            //Vector3 dirToPlayer = transform.position - Player.transform.position;
+            //Vector3 newPos = transform.position + dirToPlayer;
+            Vector3 newPos = Player.transform.position;
+            agent.SetDestination(newPos);
+        }
+	}
+}
---
+++ Assets/Player/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+    public float basicAttackDamage = 5;
+    public float basicAttackSpeed = 1.0f;
+    public float basicHealth = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
