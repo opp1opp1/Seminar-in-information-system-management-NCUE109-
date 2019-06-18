5fa738cf60192e78375e25352e8b5a557065633c
yen861112 <33426923+yen861112@users.noreply.github.com>
2019-06-18 20:42:22 +0800

    Stage0102 Adjust
    delete multiple components in stage0102
    add SceneChagnerVictory

---
+++ Assets/SceneChanger/SceneChangerVictory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerVictory : MonoBehaviour {
+
+    private Transform p_transform;
+
+	void Start()
+    {
+        p_transform = this.transform;
+    }
+
+    void Update()
+    {
+        if (p_transform.position.z > 4)
+            SceneManager.LoadScene("MainScene");
+    }
+}
