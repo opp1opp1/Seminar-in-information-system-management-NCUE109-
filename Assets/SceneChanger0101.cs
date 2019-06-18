bcacc6510e17f85a6887228dc0a7ebb99442766e
yen861112 <33426923+yen861112@users.noreply.github.com>
2019-06-18 13:19:43 +0800

    Scene Change
    MainScene to Stage0101
    Stage0101 click ESC back to MainScene

---
+++ Assets/SceneChanger0101.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger0101 : MonoBehaviour {
+
+    public void Stage0101()
+    {
+        SceneManager.LoadScene("Stage0101");
+    }
+}
