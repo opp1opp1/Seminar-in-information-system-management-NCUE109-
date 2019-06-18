608da9669d328f0ca461c8f73e7f1d88053a840b
Kevinkang0211 <45477381+Kevinkang0211@users.noreply.github.com>
2019-06-18 14:18:34 +0800

    增加一個enemystat
    enemystat用以記錄敵人血量

---
+++ Assets/Enemy/EnemyStat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStat : MonoBehaviour {
+    public int enemytype; //用以判斷enemy的種類
+    public float enemyhealth; //enemy的血量
+
+
+	// Use this for initialization
+	void Start () {
+
+        switch (enemytype) //根據敵人種類，有不同的血量
+            {
+                case 1:
+                    enemyhealth = 100;
+                    break;
+                case 2:
+                    enemyhealth = 60;
+                    break;
+                default:
+                    Debug.Log("enemy dont have health");
+                    break;
+            }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
