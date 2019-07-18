d0d79749727191c648bd8169b408cfe4c8ac864e
Bosh Shieh <opp1opp1@yahoo.com.tw>
2019-07-18 10:26:00 +0800

    護頓現在會根據最大生命值逐漸遞減了

--- Assets/Player/PlayerStats.cs
+++ Assets/Player/PlayerStats.cs
@@ -12,15 +12,35 @@ public class PlayerStats : MonoBehaviour {
     public float currentSheild = 0;
     private GameObject bullet;
     private float bulletdamage;
+    private float removedshieldtime = 0.5f;
+    private float removedshieldtimer; 
     // Use this for initialization
     void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+        removedshieldtimer = removedshieldtime;
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentSheild > 0f)
+        {
+            removedshieldtimer -= Time.deltaTime;
+            if (removedshieldtimer <= 0f)
+            {
+                if (currentSheild > basicHealth * 0.02f)
+                {
+                    currentSheild -= basicHealth * 0.02f;
+                    Debug.Log("Sheild:" + currentSheild);
+                }
+                else
+                {
+                    currentSheild = 0f;
+                    Debug.Log("Sheild:" + currentSheild);
+                }
+                removedshieldtimer = removedshieldtime;
+            }
+        }
+    }
     
 
 }
