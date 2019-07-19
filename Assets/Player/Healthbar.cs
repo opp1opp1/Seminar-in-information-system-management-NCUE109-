aca6e79762922781b93ed4bceea419c88b1de0b2
Kevinkang0211 <45477381+Kevinkang0211@users.noreply.github.com>
2019-07-19 22:12:12 +0800

    新增防護盾的UI

--- Assets/Player/Healthbar.cs
+++ Assets/Player/Healthbar.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Healthbar : MonoBehaviour {
     public float maxHP;
     public float currentHP;
+    private float currentSheild;
     private GameObject target;
     // Use this for initialization
     void Start () {
@@ -15,6 +16,7 @@ public class Healthbar : MonoBehaviour {
 	void Update () {
         
         currentHP = target.GetComponent<PlayerStats>().currentHealth;
-        this.transform.localPosition = new Vector3(-105+105*(currentHP/maxHP), 0.0f, 0.0f);
+        currentSheild = target.GetComponent<PlayerStats>().currentSheild;
+        this.transform.localPosition = new Vector3(-155+105*(currentHP/maxHP+currentSheild/100), 0.0f, 0.0f);
 	}
 }
