6b9284c461c9fb0a1b2b21ad5583909d8146f59b
yen861112 <33426923+yen861112@users.noreply.github.com>
2019-09-25 16:16:33 +0800

    FButtonInstantiate

--- Assets/FloatingUI/FButtonInstantiate.cs
+++ Assets/FloatingUI/FButtonInstantiate.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FButtonInstantiate : MonoBehaviour {
-    GameObject FButton1;
+    public GameObject Instantiate_FButton1;    //物件生成點
+    public GameObject FButton1;    //要生成的物件
 	// Use this for initialization
-	void Start () {
-		
+	void Start () { //一開始就執行生成物件
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (FButton1.gameObject.activeSelf)
+        {
+            Instantiate(FButton1, Instantiate_FButton1.transform.position,  //生成(物件,位置,旋轉值)
+            Instantiate_FButton1.transform.rotation);                       //Instantiate實例化
+            Debug.Log("hit");
+        }
 	}
 }
