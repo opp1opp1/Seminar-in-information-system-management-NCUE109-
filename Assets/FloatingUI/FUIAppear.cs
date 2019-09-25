56b8c56b17d5ab38e00df00a61f5aaf64c44d5eb
yen861112 <33426923+yen861112@users.noreply.github.com>
2019-09-25 15:25:43 +0800

    FUIAppear註解 FButtonInstantiate

--- Assets/FloatingUI/FUIAppear.cs
+++ Assets/FloatingUI/FUIAppear.cs
@@ -7,21 +7,20 @@ public class FUIAppear : MonoBehaviour {
     GameObject gameFPanel;
 	// Use this for initialization
 	void Start () {
-        gameFPanel = GameObject.Find("FPanel");
-        gameFPanel.SetActive(false);
+        gameFPanel = GameObject.Find("FPanel"); //找到FPanel
+        gameFPanel.SetActive(false);    //先設置FPanel隱藏
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    	
 	}
-    private void OnTriggerEnter(Collider col)
+    private void OnTriggerEnter(Collider col)   //碰撞事件:用Trigger和Collider，因為是要碰撞到可穿透的物件
     {
-        if(col.gameObject.name == "powerup(Clone)")
+        if(col.gameObject.name == "powerup(Clone)") //如果Ashe撞到powerup
         {
             
-            gameFPanel.gameObject.SetActive(true);
-            Debug.Log("hi");
+            gameFPanel.gameObject.SetActive(true);  //產生FPanel
         }
     }
 }
