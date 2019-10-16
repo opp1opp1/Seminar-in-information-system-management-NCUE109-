301bfd2ce39fbc4184e81faa93652e1159027ab1
yen861112 <33426923+yen861112@users.noreply.github.com>
2019-10-16 16:41:56 +0800

    繼續調整介面

--- Assets/FloatingUI/FButtonInstantiate0.cs
+++ Assets/FloatingUI/FButtonInstantiate0.cs
@@ -2,30 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FButtonInstantiate0 : MonoBehaviour {
-    public GameObject[] Objects;
-    public Transform[] Points;
-    public GameObject clone0;    //複製出來的物件
+public class FButtonInstantiate0 : MonoBehaviour {  //隨機生成三個指定地點的隨機物件
+    public GameObject point_01; //三個指定地點
+    public GameObject point_02;
+    public GameObject point_03;
+    public GameObject[] Objects;    //隨機選擇物件
+    public GameObject clone_01; //複製出來的物件
+    public GameObject clone_02; //複製出來的物件
+    public GameObject clone_03; //複製出來的物件
     public GameObject gameFPanel;   //複製出來後，要放到FPanel的層級底下
+    private bool stop = true;   //避免無線複製，用來控制按鈕被點擊後立刻再生
 
     // Use this for initialization
     void Start () {
         gameFPanel = GameObject.Find("FPanel"); //拿來把clone放到FPanel底下之用
-        Ins_Objs();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        Ins_Objs1();
+        Ins_Objs2();
+        Ins_Objs3();
+    }
+
+    // Update is called once per frame
+    void Update () {    //if stop = true，再生; stop = false，消滅
 		
 	}
 
-    void Ins_Objs()
+    void Ins_Objs1()    //生成第一個點的物件
+    {
+        int Random_Objects = Random.Range(0, Objects.Length);   //隨機挑一個物件
+        clone_01 = Instantiate(Objects[Random_Objects], point_01.transform.position,    //生成物件
+            point_01.transform.rotation);
+        clone_01.transform.parent = gameFPanel.transform;  //複製出FButton後，將他放到FPanel底下，才顯現得出來
+    }
+
+    void Ins_Objs2()
     {
         int Random_Objects = Random.Range(0, Objects.Length);
-        int Random_Points = Random.Range(0, Points.Length);
-        clone0 = Instantiate(Objects[Random_Objects], Points[Random_Points].transform.position, 
-            Points[Random_Points].transform.rotation);
-        clone0.transform.parent = gameFPanel.transform;  //複製出FButton後，將他放到FPanel底下，才顯現得出來
-        Debug.Log("hitme");
+        clone_02 = Instantiate(Objects[Random_Objects], point_02.transform.position,
+            point_02.transform.rotation);
+        clone_02.transform.parent = gameFPanel.transform;  //複製出FButton後，將他放到FPanel底下，才顯現得出來
+    }
+
+    void Ins_Objs3()
+    {
+        int Random_Objects = Random.Range(0, Objects.Length);
+        clone_03 = Instantiate(Objects[Random_Objects], point_03.transform.position,
+            point_03.transform.rotation);
+        clone_03.transform.parent = gameFPanel.transform;  //複製出FButton後，將他放到FPanel底下，才顯現得出來
     }
 }
