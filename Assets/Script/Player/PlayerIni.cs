cdc1397cb0e61a6af67ffc72e22a318ed5822f1e
Kevinkang0211 <45477381+Kevinkang0211@users.noreply.github.com>
2019-10-30 16:21:23 +0800

    Update PlayerIni.cs

--- Assets/Script/Player/PlayerIni.cs
+++ Assets/Script/Player/PlayerIni.cs
@@ -2,26 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class PlayerIni {
+public static class PlayerIni
+{
     public static float basicHealth = 100;
     public static float basicSheild = 100;
     public static float currentHealth = 100;
-    public static float currentSheild = 0;
+    public static float currentSheild = 100;
     public static float currentAttackDamage = 20;
     public static float currentAttackSpeed = 1.0f;
     public static float currentHealthLimit = 100;
 
-    public static bool Muitishot = true; //連續射擊
-    public static bool MuitishotSecondChecker = true; //連續射擊的第二發判斷器
+    public static bool Muitishot = false; //連續射擊
+    public static bool MuitishotSecondChecker = false; //連續射擊的第二發判斷器
     public static bool FrontArrow = false; //齊射
     public static bool DiagonalArrow = false; //多重射擊
     public static bool IceArrow = false; //冰屬性
     public static bool FireArrow = false; //冰屬性
     public static bool WindArrow = false; //風屬性
-    public static bool EarthArrow = false; //土屬性
+    public static bool EarthArrow = true; //土屬性
 
-   
-    
+
+
 
 
 }
