c4425879201c78b299ec7fd7a2a677810aaeb624
Bosh Shieh <opp1opp1@yahoo.com.tw>
2019-11-06 10:53:06 +0800

    元素按鈕

---
+++ Assets/Elementalpower.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Elementalpower : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnClick()
+    {
+        if (this.gameObject.name == "earth(Clone)")
+        {
+            PlayerIni.EarthArrow = true;
+            Debug.Log("Yes! Earth is on!!!");
+        }
+        if (this.gameObject.name == "fire(Clone)")
+        {
+            PlayerIni.FireArrow = true;
+            Debug.Log("Yes! Fire is on!!!");
+        }
+        if (this.gameObject.name == "ice(Clone)")
+        {
+            PlayerIni.IceArrow = true;
+            Debug.Log("Yes! Ice is on!!!");
+        }
+        if (this.gameObject.name == "wind(Clone)")
+        {
+            PlayerIni.WindArrow = true;
+            Debug.Log("Yes! Wind is on!!!");
+        }
+    }
+}
