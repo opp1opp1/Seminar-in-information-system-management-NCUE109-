c2cc5ad6ab7c4a0e436ed68e1c3db310d288c380
Kevinkang0211 <45477381+Kevinkang0211@users.noreply.github.com>
2019-12-04 13:48:02 +0800

    場景2-7暫停按鈕功能+設定頁面
    暫停按鈕重第一關開始 後面都可以正常執行\

---
+++ Assets/Music_button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_button : MonoBehaviour {
+
+    private AudioSource bgMusicAudioSource;
+    bool isON = true;
+    private void Awake()
+    {
+        //在所有Game Object中找尋Background Music
+        bgMusicAudioSource = GameObject.FindGameObjectWithTag("Background Music").GetComponent<AudioSource>();
+    }
+    public void musicbutton()
+    {
+
+        if (isON)
+        {
+
+            //暫停音樂
+            bgMusicAudioSource.Stop();
+            Time.timeScale = 0;
+            isON = false;
+
+        }
+        else
+        {
+
+            //繼續音樂
+            bgMusicAudioSource.Play();
+
+            Time.timeScale = 1;
+            isON = true;
+        }
+    }
+}
---
+++ Assets/Sound_effect_button.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_effect_button : MonoBehaviour {
+    bool isON = true;
+
+}
