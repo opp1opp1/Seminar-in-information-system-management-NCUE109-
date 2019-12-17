79ec540f7b09326dcd5d2c22b676048be7e9b14f
Bosh Shieh <opp1opp1@yahoo.com.tw>
2019-12-17 16:48:38 +0800

    教學關更新

--- Assets/FloatingUI/FUIAppear.cs
+++ Assets/FloatingUI/FUIAppear.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FUIAppear : MonoBehaviour {//套在Ashe身上
     public GameObject gameFPanel;
+    public GameObject Mask;
     public bool isPaused = false;    //讓update裡的動作暫停
     public GameObject stop_button; //用來在選擇能力時將暫停鈕隱藏，避免選擇能力時觸碰到暫停鈕
     public int st;
@@ -21,10 +22,21 @@ public class FUIAppear : MonoBehaviour {//套在Ashe身上
 	}
     private void OnTriggerEnter(Collider col)   //碰撞事件:用Trigger和Collider，因為是要碰撞到可穿透的物件
     {
-        if(col.gameObject.name == "powerup(Clone)"|| col.gameObject.name == "powerup(Tutorial)") //如果Ashe撞到powerup
+        if(col.gameObject.name == "powerup(Clone)") //如果Ashe撞到powerup
         {
             gameFPanel.GetComponent<FButtonInstantiate0>().stop = true; //吃到powerup時則會出現panel
             gameFPanel.gameObject.SetActive(true);  //顯示FPanel
+            Vector3 st = stop_button.transform.position;    //暫時隱藏暫停鈕
+            st = new Vector3(st.x - 1000, st.y, st.z);
+            stop_button.transform.position = st;
+            Destroy(col.gameObject);    //摧毀powerup
+            PauseGame();    //暫停遊戲
+        }
+        if (col.gameObject.name == "powerup(Tutorial)(Clone)")
+        {
+            gameFPanel.GetComponent<FButtonInstantiate0>().stop = true; //吃到powerup時則會出現panel
+            gameFPanel.gameObject.SetActive(true);  //顯示FPanel
+            Mask.gameObject.SetActive(true);
             Vector3 st = stop_button.transform.position;    //暫時隱藏暫停鈕
             st = new Vector3(st.x - 1000, st.y, st.z);
             stop_button.transform.position = st;
