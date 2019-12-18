0556ab0d8bd2da38739d86bd5f92682b346ff6e7
Bosh Shieh <opp1opp1@yahoo.com.tw>
2019-12-18 14:20:05 +0800

    血量

---
+++ Assets/HpScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;    // 記得加這行
+
+public class HpScript : MonoBehaviour {
+    public GameObject Ashe;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Text>().text = "" + Ashe.GetComponent<PlayerStats>().currentHealth;
+    }
+}
