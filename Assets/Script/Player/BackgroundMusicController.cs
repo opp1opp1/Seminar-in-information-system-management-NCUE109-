5d2d1199254aa1c789b290a925eb97e42a82c5d6
yen861112 <33426923+yen861112@users.noreply.github.com>
2019-12-25 17:10:38 +0800

    整理

--- Assets/Script/Player/BackgroundMusicController.cs
+++ Assets/Script/Player/BackgroundMusicController.cs
@@ -9,19 +9,13 @@ public class BackgroundMusicController : MonoBehaviour {
     AudioSource mm;
 	// Use this for initialization
     void Awake()
-    {
-        
+    {       
         bm = GameObject.FindGameObjectWithTag("Background Music").GetComponent<AudioSource>();
         bm.enabled = true;
     }
 	void Start () {
         mm = GameObject.FindGameObjectWithTag("Main Music").GetComponent<AudioSource>();
         mm.enabled = false;
-        /*foreach (var dontobj in Background Music)
-        {
-            GameObject.Find("Background Music").SetActive(true);
-        }*/
-
     }
 	
 	// Update is called once per frame
