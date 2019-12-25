15706c9b1d0101d0efedc0c8f9646d1b7f3265d7
yen861112 <33426923+yen861112@users.noreply.github.com>
2019-12-25 17:23:16 +0800

    mainmute等等來救你

---
+++ Assets/Scenes/MDontDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MDontDestroy : MonoBehaviour {
+    private static MDontDestroy instance = null;
+    public static MDontDestroy Instance
+    {
+        get { return instance; }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
